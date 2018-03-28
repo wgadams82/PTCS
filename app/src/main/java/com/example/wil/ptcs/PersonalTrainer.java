98a87661f1ba4150bd9f448483b7d8c0fd672589
Wil <wgadams82@gmail.com>
2018-03-28 18:46:06 -0400

    3/28/2018

---
+++ app/src/main/java/com/example/wil/ptcs/PersonalTrainer.java
@@ -0,0 +1,22 @@
+package com.example.wil.ptcs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class PersonalTrainer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_trainer);
+
+        //mLoginButton = (Button) findViewById(R.id.login_button);
+       // mTrueButton.setOnClickListener(new View.OnClickListener()){
+            //@Override
+                   // public void onClick(View v) {
+
+           // }
+       // }
+    }
+}
